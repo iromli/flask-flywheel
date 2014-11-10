8c2595fe04a7128e139a5ccea92dc26634194866
Isman Firmansyah <isman.firmansyah@gmail.com>
2014-11-10 21:30:26 +0700

    add documentation in the sourcecode

--- flask_flywheel/__init__.py
+++ flask_flywheel/__init__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from __future__ import (
     absolute_import,
     division,
@@ -10,11 +11,82 @@ from flywheel import Engine as _Engine
 
 
 class Engine(_Engine):
+    """A subclass of ``flywheel.Engine`` that adds additional features.
+    """
+
     def connect(self, region, **kwargs):
+        """Connect to AWS region.
+        """
         self.dynamo = DynamoDBConnection.connect(region, **kwargs)
 
 
 class Flywheel(object):
+    """This class is used to control the Flywheel integration to Flask
+    application.
+
+    This class accepts various configuration to be used to customize its
+    behavior, e.g. connecting to DynamoDB local instance:
+
+    FLYWHEEL_DATABASE_HOST:
+        DynamoDB host. Set the value (e.g. ``"localhost"``) to connect to
+        local instance. Default to ``None``.
+
+    FLYWHEEL_DATABASE_PORT:
+        DynamoDB port. Set the value (e.g. 8000) to connect to local instance.
+        Default to ``None``.
+
+    FLYWHEEL_REGION:
+        AWS region. If connected to local instance, this value is ignored.
+        Default to ``"us-east-1"``.
+
+    FLYWHEEL_SECURE:
+        Determine whether the connection must use secure protocol (HTTPS).
+        Default to ``True``.
+
+    AWS_ACCESS_KEY:
+        AWS access key. Default to ``None``.
+
+    AWS_SECRET_ACCESS_KEY:
+        AWS secret access key. Default to ``None``.
+
+    There are two usage modes which work very similar. One is binding
+    the instance to a very specific Flask application::
+
+        from flask import Flask
+
+        app = Flask(__name__)
+        db = Flywheel(app)
+
+    The second possibility is to create the object once and configure
+    application later to support it::
+
+        db = Flywheel()
+
+        def create_app():
+            app = Flask(__name__)
+            db.init_app(app)
+            return app
+
+    Since this class acts as a thin layer of ``flywheel`` library,
+    to declare a model and its field, use ``flywheel.Model`` and
+    ``flywheel.Field`` directly::
+
+        from flywheel import Model, Field
+
+        class User(Model):
+            username = Field(hash_key=True)
+
+    Afterwards, instead of using the original ``flywheel.Engine`` to
+    interact with DynamoDB, use the ``engine`` attribute provided by
+    this class::
+
+        db.engine.register(User)
+        db.engine.create_schema()
+
+    Refer to `flywheel documentation <http://flywheel.readthedocs.org/>`_
+    for details on how to interact with DynamoDB.
+    """
+
     def __init__(self, app=None):
         self._engine = None
         self.app = app
@@ -23,6 +95,9 @@ class Flywheel(object):
             self.init_app(app)
 
     def init_app(self, app):
+        """This callback can be used to initialize an application for the
+        use with this database setup.
+        """
         app.config.setdefault("FLYWHEEL_DATABASE_HOST", None)
         app.config.setdefault("FLYWHEEL_DATABASE_PORT", None)
         app.config.setdefault("FLYWHEEL_REGION", "us-east-1")
@@ -36,6 +111,10 @@ class Flywheel(object):
 
     @property
     def engine(self):
+        """Returns a cached object of DynamoDB connection. It's worth
+        noting that accessing this property without having a proper
+        initialization step will raise an error.
+        """
         assert self.app is not None, \
             "The flywheel extension was not registered " \
             "to the current application. Please make sure " \
